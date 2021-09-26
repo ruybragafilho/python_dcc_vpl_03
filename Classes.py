f0ae5166667284c80688e35b9cb768d592627d23
ruybragafilho <ruybragafilho@gmail.com>
2021-09-25 22:42:16 -0300

    Commit Inicial

---
+++ Classes.py
@@ -0,0 +1,19 @@
+class Item:
+    #Adicione seu codigo aqui
+    pass
+
+class Livro:
+    #Adicione seu codigo aqui
+    pass   
+    
+class Brinquedo:
+    #Adicione seu codigo aqui
+    pass    
+
+class Eletronico:
+    #Adicione seu codigo aqui
+    pass    
+    
+class CestaCompras:
+    #Adicione seu codigo aqui
+    pass    
